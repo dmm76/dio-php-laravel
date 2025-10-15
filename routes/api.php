6227141295da90093e890dda276a6b71c34600a3
Douglas Marcelo Monquero <douglas.monquero@gmail.com>
2025-10-15 16:40:01 -0300

    add rota api get e post que recebe variavel

--- routes/api.php
+++ routes/api.php
@@ -8,6 +8,18 @@ Route::get('hello', function () {
     return 'Hello, World!';
 });
 
+Route::get('hello/{name}', function ($name) {
+    return 'Hello ' . $name . '!';
+});
+
+Route::post('hello-post', function () {
+    return 'Hello, POST World!';
+});
+
+Route::post('hello-post/{name}', function ($name) {
+    return 'Hello, ' . $name . ' POST World!';
+});
+
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
