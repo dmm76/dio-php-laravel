161a91f0818456ed6cbd90726f442e4ee1aa763e
Douglas Marcelo Monquero <douglas.monquero@gmail.com>
2025-10-15 17:41:37 -0300

    ajustando Controller para retornar um json

--- app/Http/Controllers/HelloWorldController.php
+++ app/Http/Controllers/HelloWorldController.php
@@ -7,21 +7,21 @@ class HelloWorldController extends Controller
 {
     public function postHello()
     {
-        return 'Hello, POST World!';
+        return response()->json("Hello, POST World!");
     }
 
     public function postHelloName($name)
     {
-        return 'Hello, ' . $name . ' POST World!';
+        return response()->json('Hello, ' . $name . ' POST World!');
     }
 
     public function hello()
     {
-        return 'Hello, World!';
+        return response()->json("Hello, World!");
     }
 
     public function helloName($name)
     {
-        return 'Hello ' . $name . '!';
+        return response()->json('Hello, ' . $name . ' World!');
     }
 }
